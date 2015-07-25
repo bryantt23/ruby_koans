402efac31e63acea8ea74cd93deeeea547dc856b
bryantt23 <bryantt23@gmail.com>
2015-07-25 04:05:57 -0700

    triangle one and two done

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,23 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+
+  if (a<=0 || b<=0 || c<=0)
+    raise TriangleError
+  end
+
+  if (a+b<=c || a+c<=b || b+c<=a)
+    raise TriangleError
+  end
+
+  if(a==b && a==c)
+    return :equilateral
+  elsif (a==b || a==c || b==c)
+    return :isosceles
+  else
+    return :scalene
+  end
+
   # WRITE THIS CODE
 end
 
