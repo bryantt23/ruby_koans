f15465711b91c924f8b328002df887818935c8c6
bryantt23 <bryantt23@gmail.com>
2015-07-25 18:30:02 -0700

    inheritance

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,19 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_accessor :values
+
+  #   code ...
+  def roll(num)
+    @values = []
+    num.times do
+      @values << rand(5)+1
+    end
+  end
+
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
@@ -41,7 +54,7 @@ class AboutDiceProject < Neo::Koan
     second_time = dice.values
 
     assert_not_equal first_time, second_time,
-      "Two rolls should not be equal"
+    "Two rolls should not be equal"
 
     # THINK ABOUT IT:
     #
